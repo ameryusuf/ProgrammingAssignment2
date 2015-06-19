69d3f5c48f6fad0af0e3f7531ed7c68c5f4d880e
ameryusuf <ameryusuf@yahoo.com>
2015-06-19 22:21:16 +0500

    Edited the comments for the assignment and functions
    Changed the description for the assignment and added comments for the
    two functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,23 @@
-## The first function "makecacheMatrix creates a special "matrix" object 
-## that can cache its inverse. The second function "cacheSolve" computes the 
-## inverse of the special "matrix" returned by "makeCacheMatrix". If  
-## the inverse has already been calculated (and the matrix has not changed),  
-## then cacheSolve retrieves the inverse from the cache.
+## This is Assigment # 2 of the R course which is part of the Data Science   
+## Specialization offerd by John Hopkins University (JHU) on Coursera.  
+##  
+## There are two functions in this file, "makeCacheMatrix" and   
+## "cacheSolve", which work in tandem to cache the inverse of a matrix to speed  
+## up potentially time-consuming computations. 
 
-## Write a short comment describing this function
+
+## The following function "makecacheMatrix" creates a special "matrix object 
+## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## The following function computes inverse of the special "matrix" returned 
+## by the function above. If the inverse has already been calculated (and the
+## matrix has not changed), then cacheSolve retrieves the inverse from the
+## cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
